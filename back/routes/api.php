3ca47cda72c32c6ebd056440328dad4db915ef09
Oleg <73128022+NogaKazaha@users.noreply.github.com>
2021-12-08 13:20:42 +0200

    Added comment for routes

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,40 +4,40 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function() {
-    Route::post('/register', 'App\Http\Controllers\AuthController@register');
-    Route::post('/login', 'App\Http\Controllers\AuthController@login');
-    Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
-    Route::post('/reset_password', 'App\Http\Controllers\AuthController@reset_password');
-    Route::post('/reset_password/{token}', 'App\Http\Controllers\AuthController@confirmation_token');
+    Route::post('/register', 'App\Http\Controllers\AuthController@register'); // Registration
+    Route::post('/login', 'App\Http\Controllers\AuthController@login'); // Login
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout'); // Logout
+    Route::post('/reset_password', 'App\Http\Controllers\AuthController@reset_password'); // Send reset password email
+    Route::post('/reset_password/{token}', 'App\Http\Controllers\AuthController@confirmation_token'); // Confirm reset with token from email
 });
 Route::prefix('events')->group(function() {
-    Route::get('/show_all', 'App\Http\Controllers\EventsController@index');
-    Route::get('/show/{id}', 'App\Http\Controllers\EventsController@show');
-    Route::post('/create', 'App\Http\Controllers\EventsController@store');
-    Route::post('/update/{event_id}', 'App\Http\Controllers\EventsController@update');
-    Route::delete('/delete/{event_id}', 'App\Http\Controllers\EventsController@destroy');
+    Route::get('/show_all', 'App\Http\Controllers\EventsController@index'); // Show all events
+    Route::get('/show/{id}', 'App\Http\Controllers\EventsController@show'); // Show event by its id
+    Route::post('/create', 'App\Http\Controllers\EventsController@store'); // Create new event
+    Route::post('/update/{event_id}', 'App\Http\Controllers\EventsController@update'); // Update event by its id
+    Route::delete('/delete/{event_id}', 'App\Http\Controllers\EventsController@destroy'); // Delete event by its id
 });
 Route::prefix('subs')->group(function() {
-    Route::post('/subscribe/event/{event_id}', 'App\Http\Controllers\SubscriptionsController@subscribeToEvent');
-    Route::post('/subscribe/organizer/{event_id}', 'App\Http\Controllers\SubscriptionsController@subscribeToOrganizer');
-    Route::get('/users/{event_id}', 'App\Http\Controllers\SubscriptionsController@showSubscribedUsers');
-    Route::get('/organizer/{event_id}', 'App\Http\Controllers\SubscriptionsController@showOrganizerEvents');
-    Route::get('/my', 'App\Http\Controllers\SubscriptionsController@showMySubs');
-    Route::delete('/delete/{event_id}', 'App\Http\Controllers\SubscriptionsController@unsubscribeFromEvent');
+    Route::post('/subscribe/event/{event_id}', 'App\Http\Controllers\SubscriptionsController@subscribeToEvent'); // Subscribe to event by event id
+    Route::post('/subscribe/organizer/{event_id}', 'App\Http\Controllers\SubscriptionsController@subscribeToOrganizer'); // Subscribe to organizer by event id
+    Route::get('/users/{event_id}', 'App\Http\Controllers\SubscriptionsController@showSubscribedUsers'); // Show all subscribed users to event using event id
+    Route::get('/organizer/{event_id}', 'App\Http\Controllers\SubscriptionsController@showOrganizerEvents'); // Show all ogganizers event by event id
+    Route::get('/my', 'App\Http\Controllers\SubscriptionsController@showMySubs'); // Show user subs using his token
+    Route::delete('/delete/{event_id}', 'App\Http\Controllers\SubscriptionsController@unsubscribeFromEvent'); // Delete subscription by event id
 });
 Route::prefix('comments')->group(function() {
-    Route::get('/show_all', 'App\Http\Controllers\CommentsController@index');
-    Route::get('/show/{id}', 'App\Http\Controllers\CommentsController@show');
-    Route::get('/event/{event_id}', 'App\Http\Controllers\CommentsController@showAllForEvent');
-    Route::post('/create/{event_id}', 'App\Http\Controllers\CommentsController@store');
-    Route::post('/update/{id}', 'App\Http\Controllers\CommentsController@update');
-    Route::delete('/delete/{id}', 'App\Http\Controllers\CommentsController@destroy');
+    Route::get('/show_all', 'App\Http\Controllers\CommentsController@index'); // Show all comments
+    Route::get('/show/{id}', 'App\Http\Controllers\CommentsController@show'); // Show comment by its id
+    Route::get('/event/{event_id}', 'App\Http\Controllers\CommentsController@showAllForEvent'); // Show comments to event by event id
+    Route::post('/create/{event_id}', 'App\Http\Controllers\CommentsController@store'); // Create new comment with use of event id
+    Route::post('/update/{id}', 'App\Http\Controllers\CommentsController@update'); // Update comment by its id
+    Route::delete('/delete/{id}', 'App\Http\Controllers\CommentsController@destroy'); //Delete comment by its id
 });
 Route::prefix('users')->group(function() {
-    Route::get('/show_all', 'App\Http\Controllers\UsersController@index');
-    Route::get('/show/{id}', 'App\Http\Controllers\UsersController@show');
-    Route::post('/update/{id}', 'App\Http\Controllers\UsersController@update');
-    Route::delete('/delete/{id}', 'App\Http\Controllers\UsersController@destroy');
+    Route::get('/show_all', 'App\Http\Controllers\UsersController@index'); // Show all users
+    Route::get('/show/{id}', 'App\Http\Controllers\UsersController@show'); // Show user by its id
+    Route::post('/update/{id}', 'App\Http\Controllers\UsersController@update'); // Update user by its id
+    Route::delete('/delete/{id}', 'App\Http\Controllers\UsersController@destroy'); //Delete user by its id
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
